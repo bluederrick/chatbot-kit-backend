bc2244b1d445cf4a38bb3219c99e9f60b47d84a4
bluederrick <derricknsirim77@gmail.com>
2025-10-17 02:16:11 +0100

    lastest changes added for database connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,30 @@
-// db.js
-import mongoose from "mongoose";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-export const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("✅ MongoDB connected...");
-  } catch (error) {
-    console.error("❌ MongoDB connection error:", error.message);
-    process.exit(1);
+import  { MongoClient, ServerApiVersion }from 'mongodb';
+const { MONGO_URI } = process.env
+console.log(MONGO_URI)
+ 
+const client = new MongoClient(MONGO_URI, {
+  maxPoolSize: 150,
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    // deprecationErrors: true,
   }
-};
+});
+
+class Database {
+  constructor(uri =MONGO_URI) {
+    this.uri = uri;
+  }
+   connect() {
+    try {
+       client.connect();
+      console.log("MongoDB connected successfully");
+    } catch (error) {
+      console.error(" MongoDB connection failed:", error.message);
+       process.exit(1); // stop the app if db connection fails
+    }
+  }
+}
+
+export { Database,client };
